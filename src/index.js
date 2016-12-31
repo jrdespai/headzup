6439987221120468385ab7655a89bb6902f06056
jrdespai <buckeyesundevil@gmail.com>
2016-12-30 21:02:38 -0700

    Adding Bootstrap

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ var hello = require('./app/hello');
 require('angular-ui-router');
 var routesConfig = require('./routes');
 
+require('angular-ui-bootstrap');
+
 require('./index.less');
 
 var app = 'app';
 module.exports = app;
 
 angular
-  .module(app, ['ui.router'])
+  .module(app, ['ui.router', 'ui.bootstrap'])
   .config(routesConfig)
   .component('app', hello);
